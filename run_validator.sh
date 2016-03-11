46bcb09c943959308b37c9bff364c12a938a9207
wilsonianb <bwilson@ripple.com>
2016-03-11 14:30:33 -0800

    Build rippled nightly docker image

--- run_validator.sh
+++ run_validator.sh
@@ -12,19 +12,14 @@ docker create -v /etc/opt/ripple --name rippled-config centos:latest
 docker create -v /var/lib/rippled --name rippled-data centos:latest
 
 # Build and run local rippled-validator image
-docker build -t local/rippled-validator .
 docker run \
   --name rippled-validator -d\
-  -p 127.0.0.1:51235:51235 \
+  --net=host \
   --volumes-from rippled-config --volumes-from rippled-data \
-  local/rippled-validator
+  bwilsonripple/rippled-validator:nightly
 
 # Run watchtower to auto-update validator
-# docker run \
-#   --name watchtower -d \
-#   -v /var/run/docker.sock:/var/run/docker.sock \
-#   centurylink/watchtower --cleanup rippled-validator
 docker run \
   --name watchtower -d \
   -v /var/run/docker.sock:/var/run/docker.sock \
-  centurylink/watchtower --no-pull --cleanup --debug -i 30 rippled-validator
+  centurylink/watchtower --cleanup rippled-validator
